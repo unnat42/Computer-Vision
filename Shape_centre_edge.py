4ac339a10151af4f3cef0d02f6d5724423f74f33
Unnat Antani <unnat.antani@gmail.com>
2019-10-12 12:51:15 +0530

    Add files via upload

---
+++ Shape_centre_edge.py
@@ -0,0 +1,34 @@
+import cv2
+import imutils
+
+image = cv2.imread("shapes_and_colors.jpg")
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray,(5,5),0)
+thresh = cv2.threshold(blurred, 60,255,cv2.THRESH_BINARY)[1]
+
+#cv2.imshow("Image",image)
+#cv2.imshow("Blurred",blurred)
+#cv2.imshow("Threshed",thresh)
+
+cnts = cv2.findContours(thresh.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)  #we have the coordinates of the contours with this command
+
+for c in cnts:
+    if cv2.contourArea(c)> 60:
+         M = cv2.moments(c)
+#    print(c)
+         cX = int(M["m10"] / M["m00"])
+         cY = int(M["m01"] / M["m00"])
+    
+    
+         cv2.drawContours(image,[c],-1,(0,255,0),2)
+         cv2.circle(image,(cX,cY),7,(255,255,255),-1)
+         cv2.putText(image,"Center",(cX-20,cY-20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2)
+    
+    cv2.imshow("Image",image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+
+
+
